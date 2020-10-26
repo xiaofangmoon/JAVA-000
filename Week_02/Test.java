a442c96e58287ceedacbf2802cf8946653337c41
xiaofang <xiaofangmoon@foxmail.com>
2020-10-26 19:04:13 +0800

    jvm调优，http请求

---
+++ Week_02/Test.java
@@ -0,0 +1,34 @@
+package Week_02;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * @author xiaofang
+ */
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .get()
+                .url("http://127.0.0.1:8808/test")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            System.out.println(response.body().string());
+        }
+    }
+
+
+}
+
+
+
